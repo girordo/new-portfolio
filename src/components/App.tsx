f8fe4535c2b56290254b22ee5cf558062c2fd714
Tarcísio Giroldo Siqueira <tsgiroldo@gmail.com>
2022-01-17 20:48:34 -0300

    :art: | another bunch of code

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,25 +2,14 @@ import avatar from '../assets/images/avatar.jpg'
 
 function App() {
   return (
-    <>
-      <section className="flex justify-center items-center w-full min-h-screen bg-stone-800">
-        <article className="flex flex-row justify-center">
-          <img src={avatar} className="mr-4 rounded-full" width="40%" />
-          <h1 className="text-2xl text-white">Hi, I'm Tarcísio</h1>
-          <span role="img" aria-label="Handshaking" className="text-2xl">
-            🤝
-          </span>
-        </article>
-      </section>
-      <section className="flex justify-center items-center w-full min-h-screen">
-        <article className="flex flex-row">
-          <h1 className="text-4xl">Hi, I'm Tarcísio</h1>
-          <span role="img" aria-label="Handshaking" className="text-4xl">
-            🤝
-          </span>
-        </article>
-      </section>
-    </>
+    <section className="flex flex-row justify-center items-center w-full min-h-screen bg-stone-800">
+      <div className="flex flex-row justify-center items-center">
+        <p className="text-4xl text-white">Hi, I'm Tarcísio</p>
+        <span role="img" aria-label="Handshaking" className="text-4xl">
+          🤝
+        </span>
+      </div>
+    </section>
   )
 }
 
