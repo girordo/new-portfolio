7e77b0e2081f87a72e099d01a4b9e2c27ddeffc4
Tarcísio Giroldo Siqueira <tsgiroldo@gmail.com>
2022-01-17 21:49:43 -0300

    :art: | ta por ai

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,15 @@
-import avatar from '../assets/images/avatar.jpg'
+import avatar from '../assets/images/avatar.png'
 
 function App() {
   return (
-    <section className="flex flex-row justify-center items-center w-full min-h-screen bg-stone-800">
+    <section className="flex flex-row justify-center items-center w-full min-h-screen bg-purple-900">
       <div className="flex flex-row justify-center items-center">
+        <div
+          className="object-contain"
+          style={{ clipPath: 'circle(50% at 50% 50%);' }}
+        >
+          <img src={avatar} />
+        </div>
         <p className="text-4xl text-white">Hi, I'm Tarcísio</p>
         <span role="img" aria-label="Handshaking" className="text-4xl">
           🤝
