3099e06c7b983b0ea0065c24c4dffd9badf48ede
Tarcísio Giroldo Siqueira <tsgiroldo@gmail.com>
2022-01-18 17:16:54 -0300

    :art: | Improving with data

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,4 +1,44 @@
 const Skills = () => {
+  const data = [
+    {
+      id: 1,
+      title: 'Languages',
+      techs: ['Javascript', 'Typescript', 'R', 'C'],
+    },
+    {
+      id: 2,
+      title: 'Front-end',
+      techs: [
+        'HTML5',
+        'CSS3',
+        'UX',
+        'Unit Testing',
+        'Accessibility',
+        'Semantics',
+      ],
+    },
+    {
+      id: 3,
+      title: 'Frameworks',
+      techs: ['React', 'React Native', 'Expo', 'Next.JS', 'Node', 'Shiny'],
+    },
+    {
+      id: 4,
+      title: 'Databases',
+      techs: ['PostgreSQL', 'MySQL', 'MongoDB', 'Firebase'],
+    },
+    {
+      id: 5,
+      title: 'CI/CD',
+      techs: ['Jenkins', 'Github Actions', 'AWS', 'GCP'],
+    },
+    {
+      id: 6,
+      title: 'Others',
+      techs: ['Linux', 'Docker', 'Git', 'Github', 'Jira', 'Scrum'],
+    },
+  ]
+
   return (
     <section
       className="flex flex-col justify-center items-center w-full min-h-screen"
@@ -11,55 +51,26 @@ const Skills = () => {
         </span>
       </h1>
       <section className="grid grid-cols-2 gap-10">
-        <section className="bg-stone-100 hover:shadow-lg transition-shadow duration-300 card">
-          <article className="card-body">
-            <h2 className="card-title">Languages</h2>
-            <div className="grid grid-cols-2 gap-2">
-              <div className=" badge badge-md badge-success">Javascript</div>
-              <div className=" badge badge-md badge-success">Typescript</div>
-              <div className=" badge badge-md badge-success">R</div>
-              <div className=" badge badge-md badge-success">C</div>
-              <div className=" badge badge-md badge-success">Ruby</div>
-            </div>
-          </article>
-        </section>
-        <section className="bg-stone-100 hover:shadow-lg transition-shadow duration-300 card">
-          <article className="card-body">
-            <h2 className="card-title">Frameworks</h2>
-            <div className="grid grid-cols-2 gap-2">
-              <div className=" badge badge-md badge-success">React</div>
-              <div className=" badge badge-md badge-success">React Native</div>
-              <div className=" badge badge-md badge-success">Expo</div>
-              <div className=" badge badge-md badge-success">Next.JS</div>
-              <div className=" badge badge-md badge-success">NodeJS</div>
-              <div className=" badge badge-md badge-success">Shiny</div>
-            </div>
-          </article>
-        </section>
-        <section className="bg-stone-100 hover:shadow-lg transition-shadow duration-300 card">
-          <article className="card-body">
-            <h2 className="card-title">Databases</h2>
-            <div className="grid grid-cols-2 gap-2">
-              <div className=" badge badge-md badge-success">PostgreSQL</div>
-              <div className=" badge badge-md badge-success">MySQL</div>
-              <div className=" badge badge-md badge-success">MongoDB</div>
-              <div className=" badge badge-md badge-success">Firebase</div>
-            </div>
-          </article>
-        </section>
-        <section className="bg-stone-100 hover:shadow-lg transition-shadow duration-300 card">
-          <article className="card-body">
-            <h2 className="card-title">CI/CD</h2>
-            <div className="grid grid-cols-2 gap-2">
-              <div className=" badge badge-md badge-success">Jenkins</div>
-              <div className=" badge badge-md badge-success">
-                Github Actions
+        {data.map(({ id, title, techs }) => (
+          <section
+            key={id}
+            className="bg-gradient-to-br from-purple-900 to-fuchsia-800 hover:shadow-lg transition-shadow duration-300 card"
+          >
+            <article className="card-body">
+              <h2 className="text-white card-title">{title}</h2>
+              <div className="grid grid-cols-2 gap-1">
+                {techs.map((item, index) => (
+                  <div
+                    key={index}
+                    className="font-semibold shadow-sm badge badge-lg badge-success"
+                  >
+                    {item}
+                  </div>
+                ))}
               </div>
-              <div className=" badge badge-md badge-success">AWS</div>
-              <div className=" badge badge-md badge-success">GCP</div>
-            </div>
-          </article>
-        </section>
+            </article>
+          </section>
+        ))}
       </section>
     </section>
   )
