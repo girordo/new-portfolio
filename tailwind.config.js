f8c1209fdaac9b5e3f09d02fabc5e82313560641
Tarcísio Giroldo Siqueira <tsgiroldo@gmail.com>
2022-01-19 11:50:51 -0300

    :ambulance: | IUIUIUIU

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './*.html'],
   darkMode: 'media', // 'media' or 'class'
   theme: {
     extend: {},
